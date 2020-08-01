5c9105f9ebd8d72e04404eeb8dbbc7ffcf38c2c3
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-08-01 11:25:26 +0200

    Download page style update

--- download.php
+++ download.php
@@ -9,14 +9,16 @@ echo "<hr>\r\n<pre>\r\n"; */
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>ITransfer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-dark text-white-50">
     <main class="container">
-        Click to download your <a href="http://itransfer/inbox/<?= $_GET["id"] ?>">file</a>
+        <div class="m-5">
+            <span class="d-inline-block align-middle m-2">Click to download your </span><a href="http://itransfer/inbox/<?= $_GET["id"] ?>" class="btn btn-secondary">file</a>
+        </div>
     </main>
 </body>
 
